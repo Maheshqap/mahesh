0ce49bd33c5a6a43708b3515d678e8c93ce5b782
vamsi krishna <vamsi@gmail.com>
2019-05-03 15:58:10 +0530

    Updated

--- GIT/src/git/Exgit.java
+++ GIT/src/git/Exgit.java
@@ -3,7 +3,9 @@ package git;
 public class Exgit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Mahesh");
+		System.out.println("Praveen");
+		System.out.println("");
 
 	}
 
